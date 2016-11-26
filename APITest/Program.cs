9a8e315b120592e6440359657edbc68570674805
ThomasWFan <ThomasW.Fan@outlook.com>
2016-11-26 14:44:27 +0800

    忽略敏感项

--- APITest/Program.cs
+++ APITest/Program.cs
@@ -19,7 +19,6 @@ namespace APITest
             var tokens = await Auth.AuthorizeAsync(username, passwd);
             Console.WriteLine(tokens.AccessToken);
             var follow = await tokens.GetMyFollowingWorksAsync();
-            Console.WriteLine(follow.Count);
             Console.ReadKey();
         }
 
