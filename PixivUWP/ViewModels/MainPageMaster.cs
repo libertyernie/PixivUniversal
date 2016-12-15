bfbe447e0b71ce481c65a934c49df98c8dc74b22
ThomasWFan <ThomasW.Fan@outlook.com>
2016-12-15 19:35:19 +0800

    开始编写主界面

---
+++ PixivUWP/ViewModels/MainPageMaster.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixivUWP.ViewModels
+{
+    public class MainPageMaster
+    {
+        public string ImageURL { get; set; }
+        public string Title { get; set; }
+        public string Caption { get; set; }
+        public string Tags { get; private set; }
+        public bool IsLiked { get; set; }
+        public string Size
+        {
+            get
+            {
+                return width.ToString() + "x" + height.ToString();
+            }
+        }
+
+        private int height;
+        private int width;
+
+        public int WorkWidth
+        {
+            set
+            {
+                width = value;
+            }
+        }
+
+        public int WorkHeight
+        {
+            set
+            {
+                height = value;
+            }
+        }
+
+        public bool R18 { get; set; }
+    }
+}
