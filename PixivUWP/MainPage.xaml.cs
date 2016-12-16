f09b9e47ec13a55a5dc0a5e31963d887d437f37f
ThomasWFan <ThomasW.Fan@outlook.com>
2016-12-16 20:19:14 +0800

    完成枢纽菜单

--- PixivUWP/MainPage.xaml.cs
+++ PixivUWP/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 //“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
@@ -32,7 +33,53 @@ namespace PixivUWP
 
         public ObservableCollection<MenuItem> menuItems = new ObservableCollection<MenuItem>()
         {
-            new MenuItem() {Symbol="",Label="今日焦点" }
+            new MenuItem() {Symbol="",Label="作品发现" },
+            new MenuItem() {Symbol="",Label="最新动态" },
+            new MenuItem() {Symbol="",Label="个人作品" },
+            new MenuItem() {Symbol="",Label="我的收藏" },
+            new MenuItem() {Symbol="",Label="下载任务" }
         };
+
+        private void MenuToggle_Click(object sender, RoutedEventArgs e)
+        {
+            var _sender = sender as ToggleButton;
+            Storyboard storyboard = new Storyboard();
+            if(_sender.IsChecked==true)
+            {
+                DoubleAnimationUsingKeyFrames animation = new DoubleAnimationUsingKeyFrames();
+                animation.EnableDependentAnimation = true;
+                EasingDoubleKeyFrame f1 = new EasingDoubleKeyFrame();
+                f1.Value = 48;
+                f1.KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromSeconds(0));
+                animation.KeyFrames.Add(f1);
+                EasingDoubleKeyFrame f2 = new EasingDoubleKeyFrame();
+                f2.EasingFunction = new PowerEase() { EasingMode = EasingMode.EaseOut, Power = 4 };
+                f2.KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromSeconds(0.3));
+                f2.Value = 256;
+                animation.KeyFrames.Add(f2);
+                Storyboard.SetTarget(animation, MainMenu);
+                Storyboard.SetTargetProperty(animation, "Width");
+                storyboard.Children.Add(animation);
+                storyboard.Begin();
+            }
+            else
+            {
+                DoubleAnimationUsingKeyFrames animation = new DoubleAnimationUsingKeyFrames();
+                animation.EnableDependentAnimation = true;
+                EasingDoubleKeyFrame f1 = new EasingDoubleKeyFrame();
+                f1.Value = 256;
+                f1.KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromSeconds(0));
+                animation.KeyFrames.Add(f1);
+                EasingDoubleKeyFrame f2 = new EasingDoubleKeyFrame();
+                f2.EasingFunction = new PowerEase() { EasingMode = EasingMode.EaseOut, Power = 4 };
+                f2.KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromSeconds(0.3));
+                f2.Value = 48;
+                animation.KeyFrames.Add(f2);
+                Storyboard.SetTarget(animation, MainMenu);
+                Storyboard.SetTargetProperty(animation, "Width");
+                storyboard.Children.Add(animation);
+                storyboard.Begin();
+            }
+        }
     }
 }
