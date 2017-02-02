f70428566da889c74fc9ce4f2b65a5e7a0b97f94
yinyue <yinyue200@outlook.com>
2017-02-02 13:01:54 +0800

    改善图片清晰度

--- PixivUWP/Pages/DetailPage/WorkDetailPage.xaml.cs
+++ PixivUWP/Pages/DetailPage/WorkDetailPage.xaml.cs
@@ -74,11 +74,12 @@ namespace PixivUWP.Pages.DetailPage
             {
                 siz.Text = "(" + Work.Height?.ToString() + "x" + Work.Width?.ToString() + ") " + new Converter.TagsToStr().Convert(Work.Tools, null, null, null).ToString();
                 fs.IsChecked = Work.IsBookMarked();
+                string url = Work is IllustWork ? Work.ImageUrls.Large : Work.ImageUrls.Medium;
                 des.Text = Work.Caption ?? string.Empty;
                 title.Text = Work.Title;
                 user.Text = Work.User.Name+"("+Work.GetCreatedDate().ToString()+")"   /* + "(创建与更新时间：" + Work.CreatedTime.LocalDateTime.ToString() + "," + Work.ReuploadedTime.ToString() + ")"*/;
                 tags.Text = new Converter.TagsToStr().Convert(Work.Tags, null, null, null).ToString();
-                using (var stream = await Data.TmpData.CurrentAuth.Tokens.SendRequestAsync(Pixeez.MethodType.GET, Work.ImageUrls.Medium))
+                using (var stream = await Data.TmpData.CurrentAuth.Tokens.SendRequestAsync(Pixeez.MethodType.GET, url))
                 {
                     var bitmap = new Windows.UI.Xaml.Media.Imaging.BitmapImage();
                     await bitmap.SetSourceAsync((await stream.GetResponseStreamAsync()).AsRandomAccessStream());
