fb42286543a9e559350b946f3440b52eb7f74fe9
ThomasWFan <ThomasW.Fan@outlook.com>
2017-04-14 20:56:37 +0800

    完成贡献者列表

--- PixivUWP/Pages/pg_Settings.xaml.cs
+++ PixivUWP/Pages/pg_Settings.xaml.cs
@@ -38,9 +38,38 @@ namespace PixivUWP.Pages
     /// </summary>
     public sealed partial class pg_Settings : Page
     {
+        string strithome="";
+        string strqqgroup="";
         public pg_Settings()
         {
             this.InitializeComponent();
+            var fileo = Windows.Storage.StorageFile.GetFileFromApplicationUriAsync(new Uri(@"ms-appx:///Contributors_ithome.txt"));
+            fileo.Completed += delegate
+              {
+                  var file = fileo.GetResults();
+                  var readstreamo = file.OpenReadAsync();
+                  readstreamo.Completed += delegate
+                    {
+                        var readstream = readstreamo.GetResults();
+                        StreamReader reader = new StreamReader(readstream.AsStream());
+                        strithome = reader.ReadToEnd();
+                        var fileo2 = Windows.Storage.StorageFile.GetFileFromApplicationUriAsync(new Uri(@"ms-appx:///Contributors_qqgroup.txt"));
+                        fileo2.Completed += delegate
+                        {
+                            var file2 = fileo2.GetResults();
+                            var readstreamo2 = file2.OpenReadAsync();
+                            readstreamo2.Completed += delegate
+                            {
+                                var readstream2 = readstreamo2.GetResults();
+                                StreamReader reader2 = new StreamReader(readstream2.AsStream());
+                                strqqgroup = reader2.ReadToEnd();
+                            };
+                        };
+                    };
+              };
+            while (strqqgroup == "") { }
+            con_ithome.Text = strithome;
+            con_qqgroup.Text = strqqgroup;
             try
             {
                 backpolicy.SelectedIndex = (int)Data.AppDataHelper.GetValue("BackgroundTransferCostPolicy");
