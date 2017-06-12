9cf1d5a10f6a36ea7fb2ddcdde81edfe7e465ad1
ThomasWFan <thomasw.fan@outlook.com>
2017-06-12 17:51:37 +0800

    实现基本的接口

--- TileBackground/TileBackground.cs
+++ TileBackground/TileBackground.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
 
 namespace TileBackground
 {
-    public sealed class TileBackground
+    public sealed class TileBackground : IBackgroundTask
     {
         public static void PassAuth(string Username,string Password)
         {
@@ -18,5 +19,10 @@ namespace TileBackground
             => (AppDataHelper.GetValue("uname") == null || AppDataHelper.GetValue("upasswd") == null) ?
                (false, "", "") :
                (true, (string)AppDataHelper.GetValue("uname"), (string)AppDataHelper.GetValue("upasswd"));
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
