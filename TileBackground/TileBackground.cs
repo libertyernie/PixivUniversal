baf3ac1a9097ba0df3fd490665854e5ff4ae7f96
ThomasWFan <thomasw.fan@outlook.com>
2017-06-12 11:11:40 +0800

    基本的授权储存

--- TileBackground/TileBackground.cs
+++ TileBackground/TileBackground.cs
@@ -8,5 +8,15 @@ namespace TileBackground
 {
     public sealed class TileBackground
     {
+        public static void PassAuth(string Username,string Password)
+        {
+            AppDataHelper.SetValue("uname", Username);
+            AppDataHelper.SetValue("upasswd", Password);
+        }
+
+        private static (bool isAuthed, string username, string password) getAuth()
+            => (AppDataHelper.GetValue("uname") == null || AppDataHelper.GetValue("upasswd") == null) ?
+               (false, "", "") :
+               (true, (string)AppDataHelper.GetValue("uname"), (string)AppDataHelper.GetValue("upasswd"));
     }
 }
