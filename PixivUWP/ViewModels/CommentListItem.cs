fd51406ad748ca09cf5cf1e5414f881d3f531a55
ThomasWFan <thomasw.fan@outlook.com>
2017-07-31 20:36:35 +0800

    编写评论对象的ViewModel

---
+++ PixivUWP/ViewModels/CommentListItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pixeez.Objects;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace PixivUWP.ViewModels
+{
+    public class CommentListItem
+    {
+        public IllustCommentObject Comment { get; set; }
+        public int LeftMargin { get; set; }
+        public BitmapImage Avatar { get; set; }
+    }
+}
