a5d0047a34eaf22474d1725a357da0c6e10e4595
ThomasWFan <thomasw.fan@outlook.com>
2017-08-02 21:24:58 +0800

    基本通知样式

---
+++ PixivUWP/Data/ToastHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Notifications;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace PixivUWP.Data
+{
+    public static class ToastHelper
+    {
+        public static void SendToast(string title = "Pixiv UWP", string content = "", string image = null, string logo = null, ToastGenericAppLogoCrop hintcrop = ToastGenericAppLogoCrop.Circle)
+        {
+            ToastVisual visual = new ToastVisual();
+            ToastBindingGeneric generic = new ToastBindingGeneric();
+            generic.Children.Add(new AdaptiveText() { Text = title });
+            generic.Children.Add(new AdaptiveText() { Text = content });
+            if (image != null) generic.Children.Add(new AdaptiveImage { Source = image });
+            if (logo != null)
+            {
+                generic.AppLogoOverride.Source = logo;
+                generic.AppLogoOverride.HintCrop = hintcrop;
+            }
+        }
+    }
+}
