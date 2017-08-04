f03ff2392bc8af7a41bbc97ce660e9bf66dc6dec
yinyue200 <yinyue200@outlook.com>
2017-08-04 14:16:09 +0800

    修改继承结构

--- PixivAPI/Objects/Comment.cs
+++ PixivAPI/Objects/Comment.cs
@@ -9,25 +9,13 @@ namespace Pixeez.Objects
         public int id { get; set; }
         public string comment { get; set; }
         public DateTime date { get; set; }
-        public class User
-        {
-            public int id { get; set; }
-            public string name { get; set; }
-            public string account { get; set; }
-            public class ProfileImageUrls
-            {
-                public string medium { get; set; }
-            }
-            public ProfileImageUrls profile_image_urls { get; set; }
-        }
         public User user { get; set; }
         public Comment parent_comment { get; set; }
     }
 
-    public class IllustCommentObject
+    public class IllustCommentObject: ListRootObject
     {
         public int total_comments { get; set; }
         public Comment[] comments { get; set; }
-        public string next_url { get; set; }
     }
 }
