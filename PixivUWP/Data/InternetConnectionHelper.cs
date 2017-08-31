91f4767cf90621a88ab700f18f2434cc843ff099
ThomasWFan <thomasw.fan@outlook.com>
2017-08-31 13:00:22 +0800

    完成网络类型判断

---
+++ PixivUWP/Data/InternetConnectionHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking.Connectivity;
+
+namespace PixivUWP.Data
+{
+    public class InternetConnectionHelper
+    {
+        public enum ConnectionType
+        {
+            WLan,
+            WWan,
+            Wired
+        }
+
+        public ConnectionType GetConnectionType()
+        {
+            ConnectionProfile profile = NetworkInformation.GetInternetConnectionProfile();
+            if (profile.IsWlanConnectionProfile)
+                return ConnectionType.WLan;
+            if (profile.IsWwanConnectionProfile)
+                return ConnectionType.WWan;
+            return ConnectionType.Wired;
+        }
+    }
+}
