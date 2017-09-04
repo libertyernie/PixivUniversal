63fea32ff630bb6f0a4c0b2cf96f89a0389aa4af
ThomasWFan <thomasw.fan@outlook.com>
2017-09-04 21:30:40 +0800

    完成自定义事件Helper

---
+++ PixivUWP/Data/CustomEventHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Services.Store.Engagement;
+
+namespace PixivUWP.Data
+{
+    internal static class CustomEventHelper
+    {
+        public enum EventType
+        {
+            Vote,
+            Log,
+            Other
+        }
+
+        public static void LogEvent(string log,
+            EventType eventType = EventType.Log)
+        {
+            string logmessage = eventType.ToString() + "_" + log;
+            StoreServicesCustomEventLogger.GetDefault().Log(logmessage);
+        }
+    }
+}
